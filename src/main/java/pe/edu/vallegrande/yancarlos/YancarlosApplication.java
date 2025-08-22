058ce4572361034e1daa4f85cf2943460499a413
Yancarlos Huacre Cardenas <131498027+YancarlosHuacreCardenas@users.noreply.github.com>
2025-08-22 17:10:23 +0000

    Prueba 1

---
+++ src/main/java/pe/edu/vallegrande/yancarlos/YancarlosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.yancarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YancarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YancarlosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/vallegrande/yancarlos/YancarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.yancarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YancarlosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
